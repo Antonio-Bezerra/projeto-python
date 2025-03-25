c83427ee84f827c9a3a18bf353ad6289059b4de4
Antonio Bezerra <bezerra3a@gmail.com>
2025-03-24 21:11:56 -0300

    criar um API para mandar sms

---
+++ app.py
@@ -0,0 +1,36 @@
+import pandas as pd
+from twilio.rest import Client
+
+# Your Account SID and Auth Token from console.twilio.com
+account_sid = "AC2e3bf0135d30e4fdccf81eff376aa1c1"
+auth_token = "a3a952d625447303d024dde7069e323f"
+client = Client(account_sid, auth_token)
+
+# Passo a passo de solução
+
+# Abrir os 6 arquivos em Excel
+lista_meses = ['janeiro', 'fevereiro', 'março', 'abril', 'maio', 'junho']
+
+for mes in lista_meses:
+    tabelas_vendas = pd.read_excel(f'{mes}.xlsx')
+    if (tabelas_vendas['Vendas'] > 55000).any():
+        vendedor = tabelas_vendas.loc[tabelas_vendas['Vendas']
+                                      > 55000, 'Vendedor'].values[0]
+        vendas = tabelas_vendas.loc[tabelas_vendas['Vendas']
+                                    > 55000, 'Vendas'].values[0]
+        print(
+            f'No mês {mes} alguém bateu a meta. Vendedor: {vendedor}, Vendas: {vendas}')
+        message = client.messages.create(
+            to="+55meunumero",
+            from_="+17656130841",
+            body=f'No mês {mes} alguém bateu a meta. Vendedor: {vendedor}, Vendas: {vendas}')
+        print(message.sid)
+
+
+# Para cada arquivo:
+
+# Verificar se algum valor na coluna vendas daquele arquivo é maior que 55.000
+
+# Se for maior que 55.000 => Enviar um sms com o Nome, o mês e as vendas do vendedor
+
+# Casa não seja maior quue 55.000 não quero ffazer nada
